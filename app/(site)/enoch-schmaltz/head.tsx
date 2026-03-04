4ba1a6a765695751bce49f1df28baf9f06ea266a
Enochas89 <blitzkriegu@gmail.com>
2026-03-04 17:16:25 -0500

    Extend Enoch Person JSON-LD with profile details

--- app/(site)/enoch-schmaltz/head.tsx
+++ app/(site)/enoch-schmaltz/head.tsx
@@ -4,6 +4,10 @@ const personJsonLd = {
   name: "Enoch Schmaltz",
   alternateName: "E. A. Schmaltz",
   url: "https://enochschmaltz.com/enoch-schmaltz",
+  image: "https://enochschmaltz.com/portrait.jpg",
+  jobTitle: "Writer and Developer",
+  description:
+    "Writer, developer, and project systems builder exploring leadership, technology, and complex project environments.",
   sameAs: [
     "https://www.wikidata.org/wiki/Q138569168",
     "https://www.linkedin.com/in/enochschmaltz",
@@ -11,6 +15,13 @@ const personJsonLd = {
     "https://substack.com/@enochschmaltz",
     "https://github.com/Enochas89",
     "https://about.me/enochschmaltz",
+  ],
+  knowsAbout: [
+    "Project Management",
+    "Systems Thinking",
+    "Leadership",
+    "Software Development",
+    "Technology",
   ],
 };
 
